bdcbbbd7e866e9059f345c059bcdea6ec891379d
An Nguyen <annguyenvan.2k@gmail.com>
2020-06-03 10:38:22 +0700

    Upload again

--- api/Vertex.java
+++ api/Vertex.java
@@ -1,5 +1,23 @@
 package api;
 
-public class Vertex {
+public class Vertex<V> {
+    private V element;
+
+    public Vertex(V element) {
+        this.element = element;
+    }
+
+    public Vertex(){
+        
+    }
+    public V getElement() {
+        return element;
+    }
+
+    public void setElement(V element) {
+        this.element = element;
+    }
+
+   
     
 }
