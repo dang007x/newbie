e16778b03322cd483be8e73b868a089b4d266e89
An Nguyen <annguyenvan.2k@gmail.com>
2020-06-03 10:11:25 +0700

    Second upload

---
+++ api/Edge.java
@@ -0,0 +1,40 @@
+package api;
+
+import java.util.Arrays;
+
+public class Edge<E, V> {
+    private Vertex<V>[] endpoints;
+    private E element;
+
+    public Edge(Vertex<V>[] endpoints, E element) {
+        this.endpoints = endpoints;
+        this.element = element;
+    }
+
+    public Edge() {
+    }
+
+    public Vertex<V>[] getEndpoints() {
+        return endpoints;
+    }
+
+    public void setEndpoints(Vertex<V>[] endpoints) {
+        this.endpoints = endpoints;
+    }
+
+    public E getElement() {
+        return element;
+    }
+
+    public void setElement(E element) {
+        this.element = element;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge [endpoints=" + Arrays.toString(endpoints) + ", element=" + element + "]";
+    }
+    
+    
+    
+}
